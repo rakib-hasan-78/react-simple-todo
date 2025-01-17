0ff3b7c6b1e605002c5bcdecc2da17ac897f5781
rakib hasan <rakibwebdev78@gmail.com>
2025-01-17 15:04:59 +0600

    add to click layout responsive done

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -16,16 +16,16 @@ const Application = () => {
 
     return (
         <section className="w-full h-screen my-10 ">
-            <div className="w-7/12 mx-auto bg-gradient-to-r from-red-200 via-pink-200
+            <div className="xl:w-6/12 lg:w-7/12 md:w-7/12 sm:w-7/12 msm:w-11/12 xsm:w-11/12 mx-auto bg-gradient-to-r from-red-200 via-pink-200
              to-pink-100 backdrop-blur-2xl inset-10 border border-pink-100 
              rounded-md shadow-md py-2 bg-opacity-10">
-                <div className="flex flex-row items-center w-8/12 mx-auto py-3 gap-2 space-x-3">
+                <div className="flex xl:flex-row lg:flex-row md:flex-col sm:flex-col msm:flex-col xsm:flex-col items-center xl:w-8/12 lg:w-8/12 msm:w-11/12 xsm:w-11/12 mx-auto py-3 gap-2 space-x-3 md:space-x-0 sm:space-x-0 msm:space-x-0 xsm:space-x-0">
                     <input
                         type="text"
                         name="add-text"
                         id="add-input"
                         className="bg-gradient-to-r from-slate-100/40
-                      to-zinc-100/65 py-1.5 w-8/12 outline-0
+                      to-zinc-100/65 py-1.5 w-8/12  md:w-11/12 sm:w-11/12 msm:w-11/12 xsm:w-11/12 outline-0
                        rounded-md text-zinc-400/70
                         font-light capitalize px-4
                          caret-zinc-400/75 inset-8"
@@ -36,7 +36,7 @@ const Application = () => {
                     <Button
                         disabled={!text.trim()}
                         btnName={`add task`}
-                        className={`${!text.trim()?'bg-gray-400 cursor-not-allowed text-red-400':'bg-sky-600 text-pink-50'}`}
+                        className={`${!text.trim()?'bg-gray-400 cursor-not-allowed text-red-400':'bg-sky-600 text-pink-50'} xl:w-4/12 lg:w-4/12 md:w-11/12 sm:w-11/12 msm:w-11/12 xsm:w-11/12`}
                         onAction={clickToAdd}  // Updated to onClick
                     />
                 </div>
